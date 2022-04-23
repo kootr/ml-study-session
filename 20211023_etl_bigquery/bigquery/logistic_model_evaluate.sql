0cfae2fe86e552f9ee12a034df3b51c8e07aa6c3
Kotaro Iwasawa <iwasawa@PCN-850.local>
2022-04-23 20:48:52 +0900

    Modify sql for etl

--- 20211023_etl_bigquery/bigquery/logistic_model_evaluate.sql
+++ 20211023_etl_bigquery/bigquery/logistic_model_evaluate.sql
@@ -4,7 +4,7 @@ SELECT
 FROM
   ML.EVALUATE(MODEL `<YOUR_DATASET_NAME>.model_titanic`, (
   SELECT
-    Pclass, Title, Sex, Age, Fare, Embarked, IsAlone, AgeClass, Survived AS label
+    Pclass, Title, Sex, Age, Fare, Embarked, IsAlone, Age_Class, Survived AS label
   FROM
     `<YOUR_DATASET_NAME>.train`
   WHERE PassengerId > 712
--- 20211023_etl_bigquery/bigquery/logistic_model_predict.sql
+++ 20211023_etl_bigquery/bigquery/logistic_model_predict.sql
@@ -4,7 +4,7 @@ SELECT
 FROM
   ML.PREDICT(MODEL `<YOUR_DATASET_NAME>.model_titanic`, (
   SELECT
-    PassengerId, Pclass, Title, Sex, Age, Fare, Embarked, IsAlone, AgeClass
+    PassengerId, Pclass, Title, Sex, Age, Fare, Embarked, IsAlone, Age_Class
   FROM
     `<YOUR_DATASET_NAME>.test`
   )
