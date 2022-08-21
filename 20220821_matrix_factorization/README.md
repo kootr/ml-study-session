fe0fc9ba8b5bf2421c9a37e543ebaa6f8b5265fc
Kotaro Iwasawa <kotaro1988@gmail.com>
2022-08-21 20:38:17 +0900

    Python file for session14 (#17)

---
+++ 20220821_matrix_factorization/README.md
@@ -0,0 +1,4 @@
+# Script for Matrix factorization
+Based on the blog post: http://www.quuxlabs.com/blog/2010/09/matrix-factorization-a-simple-tutorial-and-implementation-in-python/#source-code
+
+Based on the script created by Albert Au Yeung (2010) in Python2 on the above post.
---
+++ 20220821_matrix_factorization/matrix_factorization_1.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+#!/usr/bin/python
+#
+# Based on the file created by Albert Au Yeung (2010) in Python2
+# Useful blog post: http://www.quuxlabs.com/blog/2010/09/matrix-factorization-a-simple-tutorial-and-implementation-in-python/#source-code
+# An implementation of matrix factorization
+#
+
+"""
+@INPUT:
+    R     : a matrix to be factorized, dimension N x M
+    U     : an initial matrix of dimension N x K (latent user)
+    V     : an initial matrix of dimension M x K (latent item)
+    K     : the number of latent features
+    steps : the maximum number of steps to perform the optimisation
+    alpha : the learning rate
+    beta  : the regularization parameter
+@OUTPUT:
+    the final matrices U and V
+"""
+
+def main():
+    R = [
+         [5,3,0,1],
+         [4,0,0,1],
+         [1,1,0,5],
+         [1,0,0,4],
+         [0,1,5,4],
+        ]
+
+    R = np.array(R)
+
+    N = len(R)
+    M = len(R[0])
+    K = 2
+
+    U = np.random.rand(N,K)
+    V = np.random.rand(M,K)
+
+    nU, nV = matrix_factorization(R, U, V, K)
+    nR = np.dot(nU, nV.T)
+    print(nR)
+    return
+
+def matrix_factorization(R, U, V, K, steps=5000, alpha=0.0002, beta=0.02):
+    V = V.T
+    for step in range(steps):
+        for i in range(len(R)):
+            for j in range(len(R[i])):
+                if R[i][j] > 0:
+                    eij = R[i][j] - np.dot(U[i,:],V[:,j])
+                    for k in range(K):
+                        U[i][k] = U[i][k] + alpha * (2 * eij * V[k][j] - beta * U[i][k])
+                        V[k][j] = V[k][j] + alpha * (2 * eij * U[i][k] - beta * V[k][j])
+        eR = np.dot(U,V)
+        e = 0
+        for i in range(len(R)):
+            for j in range(len(R[i])):
+                if R[i][j] > 0:
+                    e = e + pow(R[i][j] - np.dot(U[i,:],V[:,j]), 2)
+                    for k in range(K):
+                        e = e + (beta/2) * ( pow(U[i][k],2) + pow(V[k][j],2) )
+        if e < 0.001:
+            break
+    return U, V.T
+
+if __name__ == "__main__":
+    main()
